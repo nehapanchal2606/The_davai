149773d8eaec0afdae5f0eb0c8b0db9526393346
unknown <pujagyanchandani@gmail.com>
2023-04-06 01:54:10 +0530

    Fix login and register issue

--- project.py
+++ project.py
@@ -2,11 +2,11 @@ from flask import *
 from flask_mysqldb import MySQL
 import re
 app = Flask(__name__)
-
+app.secret_key = 'mysecretkey'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'tha_davai_db'
+app.config['MYSQL_DB'] = 'the_davai_db'
 
 mysql = MySQL(app)
 
@@ -20,7 +20,7 @@ def index():
         contact = request.form.get('contact')
         email = request.form.get('email')
         password = request.form.get('password')
-        cursor.execute('INSERT INTO userprofile VALUES (NULL,%s,%s,%s,%s,%s,%s)',(username,city,contact,email,password,))
+        cursor.execute('INSERT INTO user VALUES (%s,%s,%s,%s,%s,%s)',(username,city,contact,email,password))
         mysql.connection.commit()
         cursor.close()
         return 'Data added'
@@ -44,7 +44,7 @@ def login():
 		
 		if users:
 			#print(" -- ",users[0]);
-			session['id'] = users[0]
+			# session['id'] = users[0]
 			session['username'] = users[1]
 			name = session['username']
 			print("--",name)
@@ -83,7 +83,7 @@ def register():
 		password = request.form.get('password')
 
 		#cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM user WHERE username = %s',(username))
+		cursor.execute('SELECT * FROM user WHERE username = %s',(username,))
 		users = cursor.fetchone()
 
 		if users:
@@ -95,10 +95,11 @@ def register():
 		elif not username or not email or not password:
 			msg = 'Please fill the form.'
 		else:
-			cursor.execute('INSERT INTO user VALUES (NULL,%s,%s,%s,%s,%s,%s)',(username,city,contact,email,password,))
-			cursor.commit()
+			cursor.execute('INSERT INTO user (username, city, contact, email, password) VALUES (%s,%s,%s,%s,%s)', (username, city, contact, email, password))
+			mysql.connection.commit()
+			# cursor.commit()
 			msg = 'Successfully Registered...'
-			return render_template('home.html') 
+			return render_template('login.html') 
 
 	elif request.method == 'POST':
 		msg = 'Please fill!'
