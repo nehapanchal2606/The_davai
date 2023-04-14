e5bb72c36bb0798e4d9a843a3f665605fd36636d
unknown <pujagyanchandani@gmail.com>
2023-04-15 02:23:39 +0530

    Removed env folder from repository

--- project.py
+++ project.py
@@ -6,7 +6,7 @@ app.secret_key = 'mysecretkey'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'tha_davai_db'
+app.config['MYSQL_DB'] = 'the_davai_db'
 
 mysql = MySQL(app)
 
